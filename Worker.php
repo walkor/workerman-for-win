cbaae3193e4567fd9cfc8099931c63d9b12174ee
walkor <walkor@users.noreply.github.com>
2017-08-28 18:05:00 +0800

    Update Worker.php

--- Worker.php
+++ Worker.php
@@ -192,7 +192,7 @@ class Worker
      * 例如 new worker('http://0.0.0.0:8080');指定使用http协议
      * @var string
      */
-    protected $protocol = '';
+    protected $protocol = null;
     
     /**
      * 当前worker实例初始化目录位置，用于设置应用自动加载的根目录
